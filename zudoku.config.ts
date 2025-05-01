1e4bc443d1902a125e2cc649e58145a3ea625a54
givtszs <givtszs@gmail.com>
2025-05-01 10:10:44 +0300

    Initial commit

---
+++ zudoku.config.ts
@@ -0,0 +1,29 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+    { id: "docs/about", label: "About" }
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Overview",
+        items: ["docs/installation", "docs/authorization", "docs/about"],
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs/installation" }],
+  apis: {
+    type: "file",
+    input: "./apis/openapi.yaml",
+    navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
+  },
+};
+
+export default config;
